eebdd3a7a40863ec2fa742b1ca6e2681e51b7752
yvette <yvetteadan2000@gmail.com>
2019-09-22 19:37:20 +0300

    Add html entries

---
+++ Desktop/my-school-projects/Java-script-project/javascript/index.js
@@ -0,0 +1,20 @@
+function userInfo()
+}
+var cc= parseInt(prompt("Enter the first two digits of the century you were born"))
+var yy= parseInt(prompt("Enter  the last two digitsof the year you were born"))
+var mm= parseInt(prompt("Enter the two digits of the month you were born"))
+var dd= parseInt(prompt("enter the digit of the day you were born"))
+
+//alert(Your Birthday is "+" "+ dd + " "+ mm +" " + cc + yy" )
+
+var day = parseInt( (cc/4) -
+
+alert("The day of the week you were born is;" + day)
+
+var gender=prompt ("Enter the number that represents your gender. 1= Female 2= Male ")
+
+if(gender===1)
+{
+ alert ("You are  Female")   
+}
+
