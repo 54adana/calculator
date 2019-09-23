897bd486d8777e70b267eff62413bf8e5775c1cb
yvette <yvetteadan2000@gmail.com>
2019-09-23 10:05:39 +0300

    Add README

--- Desktop/my-school-projects/Java-script-project/javascript/index.js
+++ Desktop/my-school-projects/Java-script-project/javascript/index.js
@@ -1,5 +1,13 @@
 function userInfo(){
+    let century=document.forms["myForm"]["century"].value
+    let year=document.forms["myForm"]["year"].value
+    let month=document.forms["myForm"]["month"].value
+    let day=document.forms["myForm"]["day"].value
+    let gender=document.forms["myForm"]["gender"].value
+
 }
+
+
 var cc= parseInt(prompt("Enter the first two digits of the century you were born"))
 var yy= parseInt(prompt("Enter  the last two digitsof the year you were born"))
 var mm= parseInt(prompt("Enter the two digits of the month you were born"))
@@ -8,15 +16,15 @@ var dd= parseInt(prompt("enter the digit of the day you were born"))
 //alert(Your Birthday is "+" "+ dd + " "+ mm +" " + cc + yy" )
 
 var day = parseInt( (cc/4) -
-alert("The day of the week you were born is;" + day)
-var gender=prompt ("Enter the number that represents your gender. 1= Female 2= Male ")
+alert("The day of the week you were born is;" + day);
+var gender=prompt ("Enter the number that represents your gender. 1= Female 2= Male ");
  
 if(gender===1)
 {
  alert ("You are  Female");  
 }
 
-eslse if (century==""){
+else if (century==""){
 alert("input the correct century");
 }
 
@@ -26,10 +34,10 @@ else if (year==""){
 }
 
 else if (month==""){
-    alert("input the correct month")
+    alert("input the correct month");
 }
-eslse if (dayofmonth=""){
-    alert("input the day of the week")
+else if (dayofmonth=""){
+    alert("input the day of the week");
 }
 
 
