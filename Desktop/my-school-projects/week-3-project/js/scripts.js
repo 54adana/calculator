0c9ef33aff39840a1b9af4243bf3686b0f7b65b8
yvette <yvetteadan2000@gmail.com>
2019-10-07 08:48:19 +0300

    add script.js insertions

--- Desktop/my-school-projects/week-3-project/js/scripts.js
+++ Desktop/my-school-projects/week-3-project/js/scripts.js
@@ -6,5 +6,26 @@ $(document).ready(function(){
         event.preventDefault();
     });
     
+        
+    }
 });
-
+function pizza(toppings, crust ,size , total , order number) {
+    this.toppings= toppings;
+    this.crust= crust;
+    this.size= size
+    this.total= total;
+    this.order number= order nember;
+    
+}
+$('btn .cart').click(function){
+     var sizePizza = $(".size option:selected").val();
+     var toppingsPizza = $(".toppings option:selected").val();
+     var crustPizza = $(".crust option:selected").val();
+     var total = parseInt(sizePizza) + parseInt(toppingsPizza) + parseInt(crustPizza);
+     var order = 1;
+     var grandTotal = 0;
+}
+$("#size").html($(".size option:selected").text() + " - " + sizePizza);
+     $("#toppings").html($(".toppings option:selected").text() + " - " + toppingsPizza);
+     $("#crust").html($(".crust option:selected").text() + " - " + crustPizza);
+     $("#total").html(total);
